12be9513a168a08dbeed389f6c3f06897ed0c8ca
Ignacio Rodriguez <ignacionr@gmail.com>
2026-06-25 12:39:13 -0300

    Implement vertical movement (VDirection and VMove) in move.h

--- src/move.h
+++ src/move.h
@@ -56,3 +56,58 @@ private:
         }
     }
 };
+
+enum VDirection
+{
+    up,
+    down
+};
+
+template <typename TCharacter>
+struct VMove
+{
+    auto direction() const { return direction_; }
+
+    void cancel()
+    {
+        isCancelled_ = true;
+    }
+
+    VMove(VDirection direction, uint32_t start_time, units::Speed const &speed, TCharacter &character)
+        : direction_{direction}
+    {
+        AddUpdate(start_time, speed, character);
+    }
+
+private:
+    VDirection direction_;
+    bool isCancelled_{false};
+
+    void AddUpdate(uint32_t start_time, units::Speed const &speed, TCharacter &character)
+    {
+        if (direction_ == VDirection::up)
+        {
+            character.addUpdate([from = character.position_, speed, &character, start_time, this](typename TCharacter::SceneType *scene) {
+                if (isCancelled_)
+                {
+                    return false;
+                }
+                auto step = units::Time{scene->age() - start_time} * speed;
+                character.position_.y = from.y - step;
+                return true;
+            });
+        }
+        else
+        {
+            character.addUpdate([from = character.position_, speed, &character, start_time, this](typename TCharacter::SceneType *scene) {
+                if (isCancelled_)
+                {
+                    return false;
+                }
+                auto step = units::Time{scene->age() - start_time} * speed;
+                character.position_.y = from.y + step;
+                return true;
+            });
+        }
+    }
+};
